f6825e1f600dacd2c2becb5d4fe1b253a63a79fb
meg19831 <meg19831@hotmail.com.ar>
2022-04-07 23:10:15 -0300

    cambios

---
+++ clase1/index.js
@@ -0,0 +1,18 @@
+/*let entrada = prompt("Ingresar una letra");
+let salida  = entrada + " " + "ingresada";
+alert(salida);*/
+
+/*ejercicio clase */
+/*let nombre = prompt ("Por favor ingresa tu nombre");
+let edad= prompt ("Por favor ingresa tu edad");
+let salida = parseInt( edad *2);
+alert (edad * 2);*/
+
+let nombre = prompt ("Por favor ingresa tu nombre");
+let edad= prompt ("Por favor ingresa tu edad");
+let salida = parseFloat( edad *2.5);
+alert (edad * 2.5);
+
+
+
+
