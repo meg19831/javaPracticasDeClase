c19c02946572b6f345d7612785ec4254048dd80c
meg19831 <meg19831@hotmail.com.ar>
2022-04-07 23:40:14 -0300

    java con parseInt

--- clase1/index.js
+++ clase1/index.js
@@ -3,15 +3,15 @@ let salida  = entrada + " " + "ingresada";
 alert(salida);*/
 
 /*ejercicio clase */
-/*let nombre = prompt ("Por favor ingresa tu nombre");
-let edad= prompt ("Por favor ingresa tu edad");
-let salida = parseInt( edad *2);
-alert (edad * 2);*/
-
 let nombre = prompt ("Por favor ingresa tu nombre");
 let edad= prompt ("Por favor ingresa tu edad");
+let salida = parseInt( edad *2);
+alert (edad * 2);
+
+/*let nombre = prompt ("Por favor ingresa tu nombre");
+let edad= prompt ("Por favor ingresa tu edad");
 let salida = parseFloat( edad *2.5);
-alert (edad * 2.5);
+alert (edad * 2.5);*/
 
 
 
