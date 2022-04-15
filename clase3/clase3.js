3e7710e758322ed10d83d103bb8685cc9bcd7b0a
meg19831 <meg19831@hotmail.com.ar>
2022-04-15 16:29:35 -0300

    agregando while

--- clase3/clase3.js
+++ clase3/clase3.js
@@ -1,9 +1,16 @@
-let turno= 1;
-let salida= "ESC";
 for (let turno= 1 ; turno <20 ; turno++) {
     let nombre= prompt ("ingresa tu nombre");
     alert (" ingresaste nombre:" + " " + nombre + " " +  "tu turno es:" + turno); 
-    }if (turno <=20){
-        alert ("mañana a las 8hs se habilitan nuevos turnos");
     }
-salida= prompt ("ingresa ESC para salir");
+    alert ("mañana a las 8hs se habilitan nuevos turnos");
+    
+
+ let salir= prompt ("escriba ESC para salir");
+ while (salir != "ESC"){
+     alert ("por favor ingrese una salida válida");
+salir= prompt ("ingresar dato salicitado para salir ESC");
+            
+             
+ }
+
+ 
