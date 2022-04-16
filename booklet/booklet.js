02f3473282adbc7a1ad50c89c5e4f431cdb30dda
meg19831 <meg19831@hotmail.com.ar>
2022-04-15 21:19:55 -0300

    agregando carpeta de booklet

---
+++ booklet/booklet.js
@@ -0,0 +1,151 @@
+/*let nombre = "Homero";
+let apellido = "Simpson";
+let edad = 40;
+console.log (nombre);
+console.log (apellido);
+console.log (edad);  */
+
+
+/* 2do ejercicio*/
+
+/*const ciudad = "Buenos Aires";
+const ciudad1= "La Plata";
+const ciudad2= "Balcarse";
+const ciudad3= "Mar del Plata";
+const ciudad4= "Miramar";  */
+
+/* console.log (ciudad);
+console.log (ciudad1)
+console.log (ciudad2)
+console.log (ciudad3)
+console.log (ciudad4) */
+
+/* 3er ejercicio */
+/*  const ciudad = "Springfield";
+let documento = "driverLicense";
+const codigo= "B47U89RE243";
+let nombre= "Bart";
+let apellido= "Simpson";
+let domicilio="742 Evergreen Terr";
+const localidad = "Sprinfield USA";
+let =fechaDeNacimiento ="02, 11, 70"; 
+let sexo = "masculino";
+let altura= 4.0;
+let colorDeOjos= "blancos";
+let peso= 85;
+
+let carnet= (nombre + " "+ apellido + "" +domicilio+""+localidad);
+console.log (carnet);
+*/
+
+//ejercicio 4
+
+/* let integrante = prompt ("nombre del integrante 1");
+let integrante1 = prompt ( "nombre del integrante 2");
+let integrante2 = prompt ("nombre del integrante 3");
+let integrante3 = prompt ( "nombre del integrante 4");
+let integrante4 = prompt ("nombre del integrante 5");
+let familia= integrante+ " " + integrante1 +" " +integrante2 + " "+integrante3+ " "  +integrante4;
+let salida = "familia";
+alert ( "Hola" + " " + salida+ " " + familia);
+console.log( familia); */
+
+//ejercicio 5 NO LO ENTENDÍ
+
+//let precio = parseInt (prompt ("ingresa el precio del producto"));
+/*let descuento30 = precio - ( precio - 30);
+let descuento20 = precio - (precio-20)
+alert ("obtuviste un descuento");
+console.log(descuento20);
+console.log(descuento30);*/
+
+//ejercicios numero 2 actividad 1
+
+
+
+/*let suma= prompt ("ingresa una suma que de como resultado mayor a 5");
+let resta = prompt ("ingresa una resta que de como resultado 3");
+
+if ((suma == 7 ) || (suma == 5) && (resta == 3)){
+    alert ( "tu respuesta es correcta");
+}else {s
+    alert ( " tu operacion no es correcta");
+}*/
+
+/* ejercicio de la segunda parte de booklet */
+// este ejercicio se realizo de otra manera pero cumple la funcion solicitada
+
+/*let nombre = prompt ("ingresar nombre");
+if ((nombre== "bart") || (nombre == "homero")){
+    alert ("fui yo");
+}else  {
+    alert ("yo no fui");
+}
+  */
+
+//actividad 2 
+/* let letra =prompt ("ingresar una letra");
+
+if ((letra == "y") || (letra== "Y")){
+    alert ="Correcto";
+}else {
+    alert ("Error");
+} */
+
+//actividad 3
+
+/*let numero= prompt ("ingresar un número");
+
+ if (numero == 1) {
+    alert ("escogiste a Homero");
+} else if (numero == 2){
+    alert ("escogiste a Marge");
+} else if (numero== 3){
+    alert ("escogiste a Bart");
+}else if (numero== 4){
+    alert ("escogiste a Lisa");
+} 
+else {
+    alert ("error");
+} */
+
+// actividad 4 presupuesto
+
+let numero=  prompt ("ingresar numero");
+if ((numero >=0) && ( numero <1000)){
+    alert (" presupuesto bajo");
+} else if ( numero >=1001 && ( numero<=3000)){
+    alert (" presupuesto medio");
+} else if( numero >3000) {
+    alert (" presupuesto alto");
+} 
+else {
+    alert ("error");
+}   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
